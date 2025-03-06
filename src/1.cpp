848b888751584ffd2da4f96156236a9ac894cc51
mechellklemm92 <mechellklemm92@gmail.com>
2025-03-06 08:16:38 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 21;
+    string name = "John";
+    double gpa = 3.5;
+    cout << "My name is " << name << ", I am " << age << " years old, and my GPA is " << gpa << endl;
+    return 0;
+}
