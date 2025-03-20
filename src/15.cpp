de8677b8a5546bcff0020f7c9a34b6016a7d0952
mechellklemm92 <mechellklemm92@gmail.com>
2025-03-20 23:45:24 +0000

    Add 15.cpp

---
+++ src/15.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int height = 5;
+    int width = 3;
+    
+    if (height > width) {
+        cout << "Height is greater than width." << endl;
+    } else {
+        cout << "Height is less than or equal to width." << endl;
+    }
+    
+    return 0;
+}
