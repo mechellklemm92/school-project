8309cbb3a945f05456179ba16c48c1602a4b06c5
mechellklemm92 <mechellklemm92@gmail.com>
2025-04-01 18:40:54 +0000

    Add 23.cpp

---
+++ src/23.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 10;
+
+    cout << "The sum is: " << (x + y) << endl;
+    cout << "The difference is: " << (y - x) << endl;
+    return 0;
+}
