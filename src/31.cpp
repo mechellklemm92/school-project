2e67cba44c0e5f8bda4b1de525a29890f77e1dc7
mechellklemm92 <mechellklemm92@gmail.com>
2025-04-08 05:34:48 +0000

    Add 31.cpp

---
+++ src/31.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 18;
+    string name = "John Doe";
+    
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    
+    return 0;
+}
