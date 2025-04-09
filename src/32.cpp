66c6fe5456c689429e2866d2bb6f6239966008ef
mechellklemm92 <mechellklemm92@gmail.com>
2025-04-09 19:42:16 +0000

    Add 32.cpp

---
+++ src/32.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
+    return 0;
+}
